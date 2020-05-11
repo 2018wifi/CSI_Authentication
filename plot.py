1d06f460a5094afdf0c2614baaedb17e0e8a4c7c
whusober <whusober@gmail.com>
2020-05-11 17:51:38 +0800

    add temporary model

--- plot.py
+++ plot.py
@@ -1,9 +1,10 @@
 from main import queue2 as queue
 from main import lock2 as lock
 
-import turtle
-
 
 def plot():
-    turtle.forward(150)
-    pass
+    while True:
+        if not queue.empty():
+            with lock:
+                data = queue.get()
+                print(data)
